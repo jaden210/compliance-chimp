eab5dae01375c8671d363de27c44c5f0fcac4bb7
jaden210 <jaden210@gmail.com>
2026-03-06 15:57:23 -0600

    fix: wire Angular router, root component, app config, and add dashboard/report components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,22 +4,19 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFunctions, getFunctions } from '@angular/fire/functions';
-
-import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
-    provideFunctions(() => getFunctions()),
   ],
 };
